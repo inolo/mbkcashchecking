ef683ec45dbc9ad6a8fee80c6b4c41e63ebbf23f
Inolo <kevin93nguyen@Yahoo.com>
2024-01-19 02:12:49 -0600

    Finished adding images for checks, customer photo, and licenses.

---
+++ cashchecking.py
@@ -0,0 +1,124 @@
+from flask import Flask, request, jsonify, Response, render_template
+import base64
+import json
+import hmac
+import hashlib
+import datetime
+import base64
+from uuid import uuid4
+import os
+import sqlite3
+import time
+import os
+import secrets
+import sys
+import requests
+import numpy as np
+from threading import Thread
+
+
+app = Flask(__name__)
+
+
+def save_image(request,url):
+    try:
+        # customerNumber = request.form['customerNumber']
+        image_data_url = request.form['imageDataUrl']
+        # checkNumber = request.form['checkNumber']
+        uuid = request.form['uuid']
+
+        with open(f'./static/images/{url}/{uuid}.png', 'wb') as file:
+            file.write(base64.b64decode(image_data_url.split(',')[1]))
+
+            return jsonify(success=True)
+    except Exception as e:
+        print(e)
+        return jsonify(success=False, error=str(e))
+
+
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    return save_image(request,'customer_photo')
+    # try:
+    #     customerName = request.form['customerName']
+    #     image_data_url = request.form['imageDataUrl']
+    #     customerPhone = request.form['phoneNumber']
+    #     uuid = request.form['uuid']
+    #
+    #     with open(f'./static/images/customer_photo/{uuid}.png', 'wb') as file:
+    #         file.write(base64.b64decode(image_data_url.split(',')[1]))
+    #
+    #     return jsonify(success=True)
+    # except Exception as e:
+    #
+    #     print(e)
+    #     return jsonify(success=False, error=str(e))
+
+@app.route('/upload_license_image', methods=['POST'])
+def upload_license_image():
+    return save_image(request, 'license_copy')
+    # try:
+    #     customerName = request.form['customerName']
+    #     image_data_url = request.form['imageDataUrl']
+    #     customerPhone = request.form['phoneNumber']
+    #     uuid = request.form['uuid']
+    #
+    #     with open(f'./static/images/license_copy/{uuid}.png', 'wb') as file:
+    #         file.write(base64.b64decode(image_data_url.split(',')[1]))
+    #
+    #     return jsonify(success=True)
+    # except Exception as e:
+    #
+    #     print(e)
+    #     return jsonify(success=False, error=str(e))
+
+@app.route('/upload_check_copy', methods=['POST'])
+def upload_check_copy():
+    return save_image(request, 'check_images')
+    # try:
+    #     image_data_url = request.form['imageDataUrl']
+    #     uuid = request.form['uuid']
+    #
+    #     with open(f'./static/images/check_images/{uuid}.png', 'wb') as file:
+    #         file.write(base64.b64decode(image_data_url.split(',')[1]))
+    #
+    #     return jsonify(success=True)
+    # except Exception as e:
+    #     print(e)
+    #     return jsonify(success=False, error=str(e))
+
+
+@app.route('/webcam')
+def webcam():
+    return render_template('webcam.html')
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/orders', methods=['GET'])
+def orders():
+    return 'This is the order page'
+
+@app.route('/customers', methods=['GET'])
+def customers():
+    return 'This is the customer page'
+
+@app.route('/reports', methods=['GET'])
+def reports():
+    return 'This is the reports page'
+
+
+@app.route('/new_order', methods=['GET'])
+def new_order():
+    uuid = uuid4().hex
+    return render_template('new_order.html',uuid=uuid)
+
+@app.route('/new_customer', methods=['GET'])
+def new_customer():
+    uuid = uuid4().hex
+    return render_template('new_customer.html',uuid=uuid)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
