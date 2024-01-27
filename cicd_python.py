93bd8335713fbb56c025e79079197df1e4df3774
Inolo <kevin93nguyen@Yahoo.com>
2024-01-27 15:33:32 -0600

    Testing type

--- cicd_python.py
+++ cicd_python.py
@@ -3,6 +3,6 @@ import subprocess
 
 
 if os.path.exists('/home/ubuntu/service_restart.txt'):
-    returned_value = subprocess.check_output('sudo systemctl restart cashchecking.sertvice', shell=True)
+    returned_value = subprocess.check_output('sudo systemctl restart cashchecking.service', shell=True)
 
 os.remove('/home/ubuntu/service_restart.txt')
