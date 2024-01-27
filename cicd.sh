5e956ad5a39858878f0cbd21318881bc27d5b442
Inolo <kevin93nguyen@Yahoo.com>
2024-01-27 12:46:47 -0600

    Wrote bash for cicd

---
+++ cicd.sh
@@ -0,0 +1,33 @@
+rm -rf /home/ubuntu/flask_app/cicd
+
+sudo mkdir -p /home/ubuntu/flask_app/cicd
+
+cd /home/ubuntu/flask_app/cicd
+
+git clone https://github.com/inolo/mbkcashchecking.git
+
+cd mbkcashchecking
+
+
+rm -rf /home/ubuntu/flask_app/cashchecking/templates
+
+mv templates /home/ubuntu/flask_app/cashchecking/templates
+
+rm /home/ubuntu/flask_app/cashchecking/cashchecking.py
+
+mv cashchecking.py /home/ubuntu/flask_app/cashchecking/cashchecking.py
+
+rm /home/ubuntu/flask_app/cashchecking/db_sql.py
+
+mv db_sql.py /home/ubuntu/flask_app/cashchecking/db_sql.py
+
+rm /home/ubuntu/flask_app/cashchecking/sql.py
+
+mv sql.py /home/ubuntu/flask_app/cashchecking/sql.py
+
+rm /home/ubuntu/flask_app/cashchecking/cicd.sh
+
+mv cicd.sh /home/ubuntu/flask_app/cashchecking/cicd.sh
+
+systemctl restart cashchecking.service
+
