9632187d215d82ebec3e2fd04a44a375a7d88ac4
Inolo <kevin93nguyen@Yahoo.com>
2024-01-27 14:30:27 -0600

    Testing webok1111212111111a1aaa11testaaaraaaa

--- cicd.sh
+++ cicd.sh
@@ -1,39 +1,39 @@
 #!/usr/bin/bash
 
-rm -rf /home/ubuntu/flask_app/cicd
+sudo rm -rf /home/ubuntu/flask_app/cicd
 
 sudo mkdir -p /home/ubuntu/flask_app/cicd
 
-cd /home/ubuntu/flask_app/cicd
+sudo cd /home/ubuntu/flask_app/cicd
 
-git clone https://github.com/inolo/mbkcashchecking.git
+sudo git clone https://github.com/inolo/mbkcashchecking.git
 
-cd mbkcashchecking
+sudo cd mbkcashchecking
 
 
-rm -rf /home/ubuntu/flask_app/cashchecking/templates
+sudo rm -rf /home/ubuntu/flask_app/cashchecking/templates
 
-mv templates /home/ubuntu/flask_app/cashchecking/templates
+sudo mv templates /home/ubuntu/flask_app/cashchecking/templates
 
-rm /home/ubuntu/flask_app/cashchecking/cashchecking.py
+sudo rm /home/ubuntu/flask_app/cashchecking/cashchecking.py
 
-mv cashchecking.py /home/ubuntu/flask_app/cashchecking/cashchecking.py
+sudo mv cashchecking.py /home/ubuntu/flask_app/cashchecking/cashchecking.py
 
-rm /home/ubuntu/flask_app/cashchecking/db_sql.py
+sudo rm /home/ubuntu/flask_app/cashchecking/db_sql.py
 
-mv db_sql.py /home/ubuntu/flask_app/cashchecking/db_sql.py
+sudo mv db_sql.py /home/ubuntu/flask_app/cashchecking/db_sql.py
 
-rm /home/ubuntu/flask_app/cashchecking/sql.py
+sudo rm /home/ubuntu/flask_app/cashchecking/sql.py
 
-mv sql.py /home/ubuntu/flask_app/cashchecking/sql.py
+sudo mv sql.py /home/ubuntu/flask_app/cashchecking/sql.py
 
-rm /home/ubuntu/flask_app/cashchecking/cicd.sh
+sudo rm /home/ubuntu/flask_app/cashchecking/cicd.sh
 
-mv cicd.sh /home/ubuntu/flask_app/cashchecking/cicd.sh
+sudo mv cicd.sh /home/ubuntu/flask_app/cashchecking/cicd.sh
 
-chmod 777 /home/ubuntu/flask_app/cashchecking/cicd.sh
+sudo chmod 777 /home/ubuntu/flask_app/cashchecking/cicd.sh
 
-systemctl restart cashchecking.service
+sudo systemctl restart cashchecking.service
 
 #testing note 1128111 please work now??///111111112132111aaaaatestaatest
 
