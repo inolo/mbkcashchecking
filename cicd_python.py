1ea5171c987935b59fdba7b3483f7ec0d823c7ba
Inolo <kevin93nguyen@Yahoo.com>
2024-01-27 16:10:01 -0600

    Python restart test

--- cicd_python.py
+++ cicd_python.py
@@ -1,8 +1,19 @@
 import os
 import subprocess
+import logging
 
+
+log_filename = '/var/log/cash_restart.log'
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S',
+                    filename=log_filename,
+                    filemode='w',
+                    force=True)
 
 if os.path.exists('/home/ubuntu/service_restart.txt'):
     returned_value = subprocess.check_output('sudo systemctl restart cashchecking.service')
+    logging.info(f"{returned_value}")
 
 os.remove('/home/ubuntu/service_restart.txt')
