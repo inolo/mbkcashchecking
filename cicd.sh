71d71eefd505d8b5314cd9826888503ad0a23f62
Inolo <kevin93nguyen@Yahoo.com>
2024-01-27 15:17:06 -0600

    Testing webok1111212111111a1aaa11testaaaraaaaaaaaqaaaaaal1aaaa

--- cicd.sh
+++ cicd.sh
@@ -18,21 +18,21 @@ sudo mv templates /home/ubuntu/flask_app/cashchecking/templates
 sudo rm /home/ubuntu/flask_app/cashchecking/cashchecking.py
 #
 sudo mv cashchecking.py /home/ubuntu/flask_app/cashchecking/cashchecking.py
-#
-#sudo rm /home/ubuntu/flask_app/cashchecking/db_sql.py
-#
-#sudo mv db_sql.py /home/ubuntu/flask_app/cashchecking/db_sql.py
-#
-#sudo rm /home/ubuntu/flask_app/cashchecking/sql.py
-#
-#sudo mv sql.py /home/ubuntu/flask_app/cashchecking/sql.py
-#
-#sudo rm /home/ubuntu/flask_app/cashchecking/cicd.sh
-#
-#sudo mv cicd.sh /home/ubuntu/flask_app/cashchecking/cicd.sh
-#
-#sudo chmod 777 /home/ubuntu/flask_app/cashchecking/cicd.sh
-#
+
+sudo rm /home/ubuntu/flask_app/cashchecking/db_sql.py
+
+sudo mv db_sql.py /home/ubuntu/flask_app/cashchecking/db_sql.py
+
+sudo rm /home/ubuntu/flask_app/cashchecking/sql.py
+
+sudo mv sql.py /home/ubuntu/flask_app/cashchecking/sql.py
+
+sudo rm /home/ubuntu/flask_app/cashchecking/cicd.sh
+
+sudo mv cicd.sh /home/ubuntu/flask_app/cashchecking/cicd.sh
+
+sudo chmod 777 /home/ubuntu/flask_app/cashchecking/cicd.sh
+
 #sudo systemctl restart cashchecking.service
 
 #testing note 1128111 please work now??///111111112132111aaaaatestaatesttry thisaa13131
