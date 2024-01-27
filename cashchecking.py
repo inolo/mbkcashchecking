19dd3b9424a2cd5ffa10e2e41a0b224f3646f673
Inolo <kevin93nguyen@Yahoo.com>
2024-01-27 14:19:20 -0600

    Testing webok1111212111111a1aaa11

--- cashchecking.py
+++ cashchecking.py
@@ -163,8 +163,8 @@ def webhooks():
         # cmd2 = "/usr/bin/python3 /home/ubuntu/flask_app/cashchecking/pythontest.py"
         # returned_value = subprocess.check_output(cmd2, shell=True)
         # logging.info(f"{returned_value}")
-        cmd = "source /home/ubuntu/flask_app/cashchecking/cicd.sh"
-        returned_value = subprocess.check_output(cmd)
+        cmd = "/usr/bin/bash /home/ubuntu/flask_app/cashchecking/cicd.sh"
+        returned_value = subprocess.check_output(cmd, shell=True)
         logging.info(f"{returned_value}")
     except Exception as e:
         logging.info(f"{e}")
