3090fc28675824ec9162040e6f30db535f25602e
Inolo <kevin93nguyen@Yahoo.com>
2024-01-27 14:08:18 -0600

    Testing webok1111212

--- cicd.sh
+++ cicd.sh
@@ -33,5 +33,5 @@ mv cicd.sh /home/ubuntu/flask_app/cashchecking/cicd.sh
 
 systemctl restart cashchecking.service
 
-#testing note 1128111 please work now??///111111
+#testing note 1128111 please work now??///11111111
 
